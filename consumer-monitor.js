73d84af47d8d1e30015edc54eadba8edbe2b36a9
Chris Troutner <chris.troutner@gmail.com>
2025-01-14 03:28:04 -0700

    Got first version of consumer monitor working

--- consumer-monitor.js
+++ consumer-monitor.js
@@ -4,10 +4,10 @@
 */
 
 // Global npm libraries
-import RetryQueue from '@chris.troutner/retry-queue'
+// import RetryQueue from '@chris.troutner/retry-queue'
 
 // Local libraries
-import config from './config/index.js'
+// import config from './config/index.js'
 import Consumer from './lib/consumer.js'
 import Nostr from './lib/nostr.js'
 
@@ -19,7 +19,7 @@ const consumerUrls = [
 // Check on the consumer instances
 setInterval(function () {
   start()
-}, 60000 * 1) // 1 minute
+}, 60000 * 60) // 1 hour
 
 async function start () {
   try {
@@ -33,17 +33,31 @@ async function start () {
 
       let report = `Report for ${consumerUrl}\n\n`
 
+      // Get the summary
+      const summaryData = await consumerCheck.getSummary()
+      // console.log(`Summary data for ${consumerUrl}: `, summaryData)
+      const summaryReport = `REST API calls in the last 24 hours: ${summaryData.status}`
+      report += summaryReport + '\n\n'
+
       // Get the top endpoints
-      const endpointData = await consumerCheck.getTopEndpoints()
+      const endpointSummary = await consumerCheck.getTopEndpoints()
       // console.log(`Endpoint data for ${consumerUrl}: `, endpointData)
-      const endpointReport = `Endpoint Popularity: \n${JSON.stringify(endpointData, null, 2)}`
+      const endpointReport = `Endpoint Popularity: \n${endpointSummary}`
       report += endpointReport + '\n\n'
 
       // Get the top IP addresses
+      const ipAddrSummary = await consumerCheck.getTopIpAddrs()
+      // console.log(`IP address data for ${consumerUrl}: `, ipAddrData)
+      const ipAddrReport = `IP Address Popularity: \n${ipAddrSummary}`
+      report += ipAddrReport + '\n\n'
 
       // Get the connected service.
 
       // Get the connected file pinning service.
+      const fileServiceData = await consumerCheck.getFileService()
+      // console.log(`File service data for ${consumerUrl}: `, fileServiceData)
+      const fileServiceReport = `File Service: ${fileServiceData.selectedIpfsFileProvider}`
+      report += fileServiceReport + '\n\n'
 
       // Post the report to Nostr.
       const nostr = new Nostr()
@@ -55,3 +69,6 @@ async function start () {
     console.error('Error in consumer-monitor.js: ', error)
   }
 }
+
+const now = new Date()
+console.log(`Consumer monitor started at ${now.toLocaleString()}.`)
